152cf032b23a94e4f959547f26e0b0cc67dae3a3
Nick Rebain <nickrebain@yahoo.com>
2017-02-18 20:28:41 -0500

    Initialization

---
+++ src/main/java/Nick3306/github/io/OptiPlot/Main.java
@@ -0,0 +1,12 @@
+package Nick3306.github.io.OptiPlot;
+
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	public void onEnable()
+	{
+		PluginManager pm = getServer().getPluginManager();
+	}
+}
